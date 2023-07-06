b74751d0142b9dbcf2f05ffa7f60077c49870914
Neal Grindstaff <91105711+Nealium104@users.noreply.github.com>
2023-07-06 13:34:36 -0400

    Neal (#10)
    * quick update about date
    * first commit and got fonts working

---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="">
+      <h1 className="text-2xl font-cabin">This is a big ol placeholder</h1>
+      <span className="font-montserrat">This is a test for fonts 🥲</span>
+    </main>
+  )
+}
