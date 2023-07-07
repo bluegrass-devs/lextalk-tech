708ee0f36711abf8def10f4d25e5bee40d4ccbf4
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-07-07 11:48:42 -0400

    added navigational bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,14 @@ module.exports = {
       fontFamily: {
         montserrat: ["var(--font-montserrat)"],
         cabin: ["var(--font-cabin)"]
-      }
+      },
+      colors: {
+        'text': '#CAD2C5',
+        'background': '#2F3E46',
+        'primary': '#52796F',
+        'secondary': '#84A98C',
+        'accent': '#354F52'
+      },
     },
   },
   plugins: [],
