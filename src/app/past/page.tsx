89f6869dd977940e0c48f5c9e5c98ba48ab49a1f
Neal Grindstaff <91105711+Nealium104@users.noreply.github.com>
2023-07-11 17:36:13 -0400

    Neal (#11)
    * quick update about date
    
    * first commit and got fonts working
    
    * added navigational bar
    
    ---------
    
    Co-authored-by: Joe Kratzat <joe.kratzat@gmail.com>

---
+++ src/app/past/page.tsx
@@ -0,0 +1,11 @@
+import Nav from "../Nav"
+
+export default function Past() {
+  return (
+    <>
+      <Nav />
+      <h1 className="text-2xl font-cabin">This is a big ol placeholder</h1>
+      <span className="font-montserrat">This is a test for fonts 🥲</span>
+    </>
+  )
+}
