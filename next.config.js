32bddc02e61505286e834b12669045f920297239
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-07-14 12:06:37 -0400

    added a hero section to the main page and a utility class

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: [
+            "placekitten.com"
+        ]
+    }
+
+}
 
 module.exports = nextConfig
