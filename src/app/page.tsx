0867a9bdbc4c5a05d4b6877e1268f659fa4c6141
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-08-26 13:11:01 -0400

    reworked the table to be more mobile friendly

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
     <>
       <main className="font-montserrat text-text">
         <Landing date={data.date} />
-        <div className="my-12 mx-auto max-w-screen-xl px-8">
+        <div className="my-12 mx-auto max-w-screen-xl px-4">
           <div className="flex items-center w-1/3 justify-between my-8">
             <h2 className="text-3xl">Schedule</h2>
             <Link
@@ -48,19 +48,35 @@ export default function Home() {
           <table className="w-full text-xl md:text-3xl">
             <thead className="">
               <tr className="text-2xl text-left">
-                <th className="px-3">Time</th>
-                <th className="px-3">Topic</th>
-                <th className="px-3">Presenter</th>
+                <th className="mx-3">Time</th>
+                <th className="mx-3">Topic</th>
+                <th className="mx-3">Presenter</th>
               </tr>
             </thead>
             <tbody>
               {data.schedule.map((line, index) => (
                 <tr key={index} className="">
-                  <td className="bg-accent p-3 font-bold">
+                  <td
+                    className={`${
+                      index % 2 == 0 ? "bg-accent" : "bg-background"
+                    } 'm-3 font-bold'`}
+                  >
                     <time>{line.time}</time>
                   </td>
-                  <td className="bg-accent p-3">{line.topic}</td>
-                  <td className="bg-accent p-3">{line.presenter}</td>
+                  <td
+                    className={`${
+                      index % 2 == 0 ? "bg-accent" : "bg-background"
+                    } 'm-3 font-bold'`}
+                  >
+                    {line.topic}
+                  </td>
+                  <td
+                    className={`${
+                      index % 2 == 0 ? "bg-accent" : "bg-background"
+                    } 'm-3 font-bold'`}
+                  >
+                    {line.presenter}
+                  </td>
                 </tr>
               ))}
             </tbody>
@@ -75,7 +91,7 @@ export default function Home() {
             objectFit="cover"
           />
         </div>
-        <div className="my-12 mx-auto max-w-screen-xl px-8">
+        {/* <div className="my-12 mx-auto max-w-screen-xl px-8">
           <h1 className="text-3xl">Event Organizers:</h1>
           <div className="flex flex-col md:flex-row justify-evenly items-center w-full">
             <div className="flex flex-col items-center w-fit my-8">
@@ -105,19 +121,20 @@ export default function Home() {
               <div className="text-2xl">This other person</div>
             </div>
           </div>
-        </div>
-        <div className="my-12 mx-auto max-w-screen-xl px-8">
+        </div> */}
+        <div className="my-12 mx-auto max-w-screen-xl px-4">
           <h2 className="text-3xl">Location:</h2>
           <div className="flex-row justify-center items-center max-w-screen-xl mx-auto my-12  md:flex">
-            <div className="w-1/2 h-96">
+            <div className="w-full md:w-1/2 h-96">
               <iframe
+                className="w-full h-full"
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3142.236842997967!2d-84.50669982410395!3d38.041567371919385!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88424522fafce13d%3A0xfec16cb4172a8bfd!2sThe%20Cornerstone!5e0!3m2!1sen!2sus!4v1691424457551!5m2!1sen!2sus"
                 width=""
                 height=""
                 loading="lazy"
               ></iframe>
             </div>
-            <div className="text-2xl mx-8">
+            <div className="text-2xl my-8 md:mx-8">
               <h3 className="text-3xl font-bold py-2">The Cornerstone</h3>
               <div>401 South Limestone</div>
               <div>Lexington, KY 40508</div>
