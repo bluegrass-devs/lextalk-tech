12ef62a52344e029b9e14e5120f5b995042a9251
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-09 14:06:35 -0400

    added basepath

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: "",
+  basePath: "https://nealium104.github.io/lextalk-tech/",
   images: {
     domains: ["placekitten.com", "res.cloudinary.com"],
     unoptimized: true,
--- src/app/team/components/Developer.tsx
+++ src/app/team/components/Developer.tsx
@@ -13,7 +13,7 @@ export const Developer: React.FC<DevProps> = ({ devData }) => {
   return (
     <div className="flex flex-col items-center justify-center my-8">
       <div className="mx-auto">
-        <img
+        <Image
           src={devData.image}
           width="500"
           height="500"
