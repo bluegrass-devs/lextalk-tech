1c26363d5ff5c34464be622630e01d1eb130427f
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-09 14:15:16 -0400

    changed path

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -1,17 +1,18 @@
 import { Developer } from "./components/Developer";
 
 export default function Team() {
+  // Unfortunately, I could only get relative paths to work.
   const devData = {
     dev: [
       {
         name: "Ember Borja",
-        image: "../../../images/conferenceTalk.jpg",
+        image: "../../../public/images/conferenceTalk.jpg",
         alt: "placeholder image",
         bio: "My name is Ember Borja. I'm a developer from Lexington, Kentucky with varied background in music and photography. I love building things that get used and ",
       },
       {
         name: "Neal Powers",
-        image: "../../../images/Neal.JPG",
+        image: "../../../public/images/Neal.JPG",
         alt: "Neal over the Seattle skyline",
         bio: "My name is Neal Powers. I'm a freelance developer from Lexington, Kentucky looking for my first role at a company! I have a varied background in music and photography and love all things JavaScript!",
       },
