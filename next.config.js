00d5db2cfc10821d27929ddcdb3dca3a2b3c651b
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-14 19:22:07 -0400

    changed config for export

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["placekitten.com"],
+    domains: ["placekitten.com", "res.cloudinary.com"],
+    unoptimized: true,
   },
+  output: "export",
 };
+
+module.exports = nextConfig;
 
 module.exports = nextConfig;
