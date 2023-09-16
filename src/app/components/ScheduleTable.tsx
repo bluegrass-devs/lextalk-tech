d871c28e6f3349faee8b46e5056fc73b80253455
Neal Grindstaff <neal.grindstaff@gmail.com>
2023-09-16 09:03:00 -0400

    changed some table styles and added some margin on talks

--- src/app/components/ScheduleTable.tsx
+++ src/app/components/ScheduleTable.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 interface ScheduleEntry {
   topic: string;
   time: string;
@@ -10,41 +12,43 @@ interface ScheduleTableProps {
 
 export const ScheduleTable: React.FC<ScheduleTableProps> = ({ data }) => {
   return (
-    <table className="w-full text-xl md:text-3xl">
-      <thead className="">
-        <tr className="text-2xl text-left">
-          <th className="mx-3">Time</th>
-          <th className="mx-3">Topic</th>
-          <th className="mx-3">Presenter</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((line, index) => (
-          <tr key={index} className="">
-            <td
-              className={`${
-                index % 2 == 0 ? "bg-accent" : "bg-background"
-              } 'm-3 font-bold'`}
-            >
-              <time>{line.time}</time>
-            </td>
-            <td
-              className={`${
-                index % 2 == 0 ? "bg-accent" : "bg-background"
-              } 'm-3 font-bold'`}
-            >
-              {line.topic}
-            </td>
-            <td
-              className={`${
-                index % 2 == 0 ? "bg-accent" : "bg-background"
-              } 'm-3 font-bold'`}
-            >
-              {line.presenter}
-            </td>
+    <Link href="/talks">
+      <table className="w-full text-xl md:text-3xl">
+        <thead className="">
+          <tr className="text-2xl text-left">
+            <th className="mx-3">Time</th>
+            <th className="mx-3">Topic</th>
+            <th className="mx-3">Presenter</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {data.map((line, index) => (
+            <tr key={index} className="hover:scale-110 duration-100">
+              <td
+                className={`${
+                  index % 2 == 0 ? "bg-accent" : "bg-background"
+                } 'm-3 font-bold'`}
+              >
+                <time>{line.time}</time>
+              </td>
+              <td
+                className={`${
+                  index % 2 == 0 ? "bg-accent" : "bg-background"
+                } 'm-3 font-bold'`}
+              >
+                {line.topic}
+              </td>
+              <td
+                className={`${
+                  index % 2 == 0 ? "bg-accent" : "bg-background"
+                } 'm-3 font-bold'`}
+              >
+                {line.presenter}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </Link>
   );
 };
