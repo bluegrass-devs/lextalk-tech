c37021fe9453fd45c28e91648ec394cffa1c7b8d
Neal Grindstaff <91105711+Nealium104@users.noreply.github.com>
2023-10-03 12:35:47 -0700

    Footer and Favicon Fix (#31)
    * changed some table styles and added some margin on talks
    
    * added that talks margin
    
    * A few styling changes in the Nav, a change to a minimum height (to keep the footer small), and I added Mariola's bio
    
    * Made past talks available on the mobile nav
    
    * fixed footer and favicon

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -4,9 +4,9 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <>
-      <footer className="text-text bg-accent font-montserrat text-sm">
+      <footer className="text-text bg-accent font-montserrat text-sm py-2 ">
         <div className="md:w-1/3 mx-auto flex flex-col flex-shrink">
-          <div className="flex flex-col md:flex-row justify-center items-center gap-4 my-2">
+          <div className="flex flex-col md:flex-row justify-center items-center gap-4">
             <Link
               href="/"
               className="my-2 w-48 text-center transition duration-150 hover:text-white hover:scale-110"
@@ -32,12 +32,18 @@ export default function Footer() {
             >
               Tickets
             </a>
-            {/* <Link
+            <Link
+              className="my-2 w-48 text-center whitespace-nowrap transition duration-150 hover:text-white hover:scale-110"
+              href="/team"
+            >
+              Team
+            </Link>
+            <Link
               href="/past"
               className="my-2 w-48 text-center whitespace-nowrap transition duration-150 hover:text-white hover:scale-110"
             >
-              Past Events
-            </Link> */}
+              Past Talks
+            </Link>
           </div>
           <div className="flex flex-col items-center">
             <div className="flex w-1/2 md:w-1/4 justify-center text-2xl my-2 gap-4">
